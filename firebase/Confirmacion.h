a53b1cd0bf2a3d92e599b5682c834d6b11479021
Silvestre Garcia <silver@Silvestres-iMac.local>
2017-10-30 22:22:34 -0600

    Se agregan clases para las vistas

---
+++ firebase/Confirmacion.h
@@ -0,0 +1,13 @@
+//
+//  Confirmacion.h
+//  firebase
+//
+//  Created by Silvestre Garcia on 10/30/17.
+//  Copyright © 2017 silver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Confirmacion : UIViewController
+
+@end
---
+++ firebase/Detalle.h
@@ -0,0 +1,13 @@
+//
+//  Detalle.h
+//  firebase
+//
+//  Created by Silvestre Garcia on 10/30/17.
+//  Copyright © 2017 silver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Detalle : UIViewController
+
+@end
